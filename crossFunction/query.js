e3860f8b68d176953adea8b8239071749cf9cba1
Bohdan <32486313+bkovalevych@users.noreply.github.com>
2020-05-08 22:21:36 +0300

    progress with query

--- crossFunction/query.js
+++ crossFunction/query.js
@@ -1,10 +1,39 @@
+function prepareValue(obj, val, key) {
+    if (typeof val !== typeof []) {
+        val = [val]
+    }
+    val.map(arrVal => {
+        if(!obj[key]) {
+            obj[key] = {}
+        }
+        if (arrVal.startsWith('$')) {
+
+            let pos = arrVal.indexOf('(');
+            let new_key = arrVal.slice(0, pos)
+            obj[key][new_key] = arrVal.slice(pos + 1, arrVal.length - 1)
+        } else {
+            if (!obj[key]['$in']) {
+                obj[key]['$in'] = []
+            }
+            obj[key]['$in'].push(arrVal)
+        }
+    })
+}
+function prepareFilter(filterQuery) {
+    let filter = {}
+    Object.keys(filterQuery).map(key => {prepareValue(filter, filterQuery[key], key)})
+    return filter;
+}
+
 module.exports = (req, res) => {
     let collection = req.collection;
-    let filter = Object.assign( {idUser: req.user._id.toString()}, req.query.filterParam);
-
     let options = Object.assign({skip: 0, limit: 100}, req.query);
-    options.skip = parseInt(options.skip);
-    options.limit = parseInt(options.limit);
+    delete req.query['skip']
+    delete req.query['limit']
+    let filter = Object.assign( {idUser: req.user._id.toString()}, prepareFilter(req.query));
+
+    options.skip = parseInt(options.skip.toString());
+    options.limit = parseInt(options.limit.toString());
 
     collection.find(filter, null , options).then(result => {
         res.json(result)
